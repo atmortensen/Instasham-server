576a0e6a91114e95f427fca28114473dc4c51062
Alexander Mortensen <alextmortensen@gmail.com>
2017-05-03 21:18:06 -0600

    adds herkoku config

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ if(!process.env.PORT){
 }
 
 app.use(bodyParser.json())
+app.set('port', (process.env.PORT || 3000))
 
 const S3_BUCKET = process.env.S3_BUCKET
 const s3 = new aws.S3()
@@ -42,4 +43,6 @@ app.get('*', function(req, res) {
 })
 
 
-app.listen(process.env.PORT || 3000)
+app.listen(app.get('port'), function(){
+  console.log('Listening on port ' + app.get('port'))
+})
